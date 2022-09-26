f392793a90b5937dbc0bda7859963d01887c8774
ratulhasan <ratuljh@gmail.com>
2022-09-26 21:57:32 +0600

    Hit start

--- wp-emoji.php
+++ wp-emoji.php
@@ -22,14 +22,75 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 final class WpEmoji{
-    public function __construct() {
 
+    /**
+     * WpEmoji construct
+     */
+    private function __construct() {
+        // Disable emojis.
+        add_action( 'init', [ $this, 'disable_emojis' ] );
+    }
+
+    /**
+     * Initializes the Workplace() class.
+     *
+     * Checks for an existing Workplace() instance
+     * and if it doesn't find one, creates it.
+     *
+     * @since 1.0.0
+     *
+     * @return WpEmoji|bool
+     */
+    public static function init() {
+        static $instance = false;
+
+        if ( ! $instance ) {
+            $instance = new WpEmoji();
+        }
+
+        return $instance;
+    }
+
+    /**
+     * Remove all hooks to disable emoji which WordPress treats like an image.
+     *
+     * @since WORKPLACE_SINCE
+     *
+     * @return void
+     */
+    public function disable_emojis() {
+        remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+        remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+        remove_action( 'wp_print_styles', 'print_emoji_styles' );
+        remove_action( 'admin_print_styles', 'print_emoji_styles' );
+        remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+        remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+        remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+        add_filter( 'wp_resource_hints', [ $this, 'disable_emojis_remove_dns_prefetch' ], 10, 2 );
+    }
+
+    /**
+     * Remove Emoji in WordPress automatically CDN hostname from DNS prefetching hints.
+     *
+     * @param array  $urls URLs to print for resource hints.
+     * @param string $relation_type The relation type the URLs are printed for.
+     *
+     * @return array Difference between the two arrays.
+     */
+    public function disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
+        if ( 'dns-prefetch' === $relation_type ) {
+            /** This filter is documented in wp-includes/formatting.php */
+            $emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
+            $urls          = array_diff( $urls, array( $emoji_svg_url ) );
+        }
+
+        return $urls;
     }
 
 }
 
 function hit_start(){
-    new WpEmoji();
+    return WpEmoji::init();
 }
 
 hit_start();
